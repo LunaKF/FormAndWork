0f5cba05c28b8a50ebf5569b2f54136de13fb3cc
LunaKF <lunakhanjifustek@gmail.com>
2025-01-26 20:22:20 +0100

    first commit

---
+++ src/main/java/com/proyecto/FormAndWork/FormAndWorkApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.FormAndWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormAndWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormAndWorkApplication.class, args);
+	}
+
+}
