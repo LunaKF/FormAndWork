861143a24fb65c54702898808b2c199d16c889de
Luna KF <lunakhanjifustek@gmail.com>
2025-05-13 16:06:31 +0200

    rafa2

--- src/main/java/com/proyecto/FormAndWork/service/CandidaturaService.java
+++ src/main/java/com/proyecto/FormAndWork/service/CandidaturaService.java
@@ -16,13 +16,11 @@ import com.proyecto.FormAndWork.entity.OfertaEntity;
 import com.proyecto.FormAndWork.repository.CandidaturaRepository;
 import com.proyecto.FormAndWork.exception.*;
 
-
 @Service
 public class CandidaturaService implements ServiceInterface<CandidaturaEntity> {
 
     @Autowired
     CandidaturaRepository oCandidaturaRepository;
-
 
     @Autowired
     AlumnoService oAlumnoService;
@@ -33,11 +31,14 @@ public class CandidaturaService implements ServiceInterface<CandidaturaEntity> {
     @Autowired
     RandomService oRandomService;
 
-    private String[] arrNombres = {"Pepe", "Laura", "Ignacio", "Maria", "Lorenzo", "Carmen", "Rosa", "Paco", "Luis",
-        "Ana", "Rafa", "Manolo", "Lucia", "Marta", "Sara", "Rocio", "Antonio", "Javier", "Cristina", "Alberto",
-        "Esteban", "David", "Fernando", "Jorge", "Raquel", "Elena", "Patricia", "Santiago", "Diego", "Victor"};
+    @Autowired
+    AuthService oAuthService;
 
-        String[] arrDescripciones = {
+    private String[] arrNombres = { "Pepe", "Laura", "Ignacio", "Maria", "Lorenzo", "Carmen", "Rosa", "Paco", "Luis",
+            "Ana", "Rafa", "Manolo", "Lucia", "Marta", "Sara", "Rocio", "Antonio", "Javier", "Cristina", "Alberto",
+            "Esteban", "David", "Fernando", "Jorge", "Raquel", "Elena", "Patricia", "Santiago", "Diego", "Victor" };
+
+    String[] arrDescripciones = {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -46,11 +47,11 @@ public class CandidaturaService implements ServiceInterface<CandidaturaEntity> {
             "Maecenas suscipit, mauris nec venenatis commodo, est erat pretium ante, id molestie eros magna at orci."
     };
 
-        @Autowired
-        SectorService oSectorService;
+    @Autowired
+    SectorService oSectorService;
 
     public Long randomCreate(Long cantidad) {
-       for (int i = 0; i < cantidad; i++) {
+        for (int i = 0; i < cantidad; i++) {
             CandidaturaEntity oAsientoEntity = new CandidaturaEntity();
             oAsientoEntity.setFecha(LocalDate.now().minusDays(oRandomService.getRandomInt(0, 30)));
             oAsientoEntity.setAlumno(oAlumnoService.randomSelection());
@@ -62,6 +63,21 @@ public class CandidaturaService implements ServiceInterface<CandidaturaEntity> {
 
     public Page<CandidaturaEntity> getPage(Pageable oPageable, Optional<String> filter) {
 
+        if (!oAuthService.isSessionActive()){
+            throw new UnauthorizedAccessException("No tienes permisos para acceder a esta información");
+        }
+
+
+        if (oAuthService.isAlumno()) {
+            // aqui un alumno va a ver las candidaturas
+            // solo puede ver las suyas
+            // habra que crear un metodo en el repository 
+            // que busque por email de alumno
+            // por lo tanto cambiar el findall 
+            // return oCandidaturaRepository.findAllByEmailDeAlumno(oPageable);
+
+        }
+
         if (filter.isPresent()) {
             return oCandidaturaRepository.findByAlumnoNombreContaining(
                     filter.get(), oPageable);
@@ -70,7 +86,7 @@ public class CandidaturaService implements ServiceInterface<CandidaturaEntity> {
         }
     }
 
-    public Page<CandidaturaEntity> getPageXoferta(Pageable oPageable, Optional<String> filter , Long id_oferta) {
+    public Page<CandidaturaEntity> getPageXoferta(Pageable oPageable, Optional<String> filter, Long id_oferta) {
 
         if (filter.isPresent()) {
             return oCandidaturaRepository.findByAlumnoNombreContaining(
@@ -79,8 +95,8 @@ public class CandidaturaService implements ServiceInterface<CandidaturaEntity> {
             return oCandidaturaRepository.findByOfertaId(id_oferta, oPageable);
         }
     }
-    
-    public Page<CandidaturaEntity> getPageXalumno(Pageable oPageable, Optional<String> filter , Long id_alumno) {
+
+    public Page<CandidaturaEntity> getPageXalumno(Pageable oPageable, Optional<String> filter, Long id_alumno) {
 
         if (filter.isPresent()) {
             return oCandidaturaRepository.findByAlumnoNombreContaining(
@@ -89,6 +105,7 @@ public class CandidaturaService implements ServiceInterface<CandidaturaEntity> {
             return oCandidaturaRepository.findByAlumnoId(id_alumno, oPageable);
         }
     }
+
     public CandidaturaEntity get(Long id) {
         return oCandidaturaRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Candidatura no encontrado"));
@@ -109,7 +126,8 @@ public class CandidaturaService implements ServiceInterface<CandidaturaEntity> {
     }
 
     public CandidaturaEntity update(CandidaturaEntity oCandidaturaEntity) {
-        CandidaturaEntity oCandidaturaEntityFromDatabase = oCandidaturaRepository.findById(oCandidaturaEntity.getId()).get();
+        CandidaturaEntity oCandidaturaEntityFromDatabase = oCandidaturaRepository.findById(oCandidaturaEntity.getId())
+                .get();
         if (oCandidaturaEntity.getFecha() != null) {
             oCandidaturaEntityFromDatabase.setFecha(oCandidaturaEntity.getFecha());
         }
@@ -128,14 +146,14 @@ public class CandidaturaService implements ServiceInterface<CandidaturaEntity> {
     }
 
     public CandidaturaEntity randomSelection() {
-        List<Long> listaIds = oCandidaturaRepository.findAllIds(); //  método para obtener los IDs añadido en el repository
+        List<Long> listaIds = oCandidaturaRepository.findAllIds(); // método para obtener los IDs añadido en el
+                                                                   // repository
         if (listaIds.isEmpty()) {
             throw new ResourceNotFoundException("No hay candidaturas disponibles para selección aleatoria");
         }
         Long idAleatorio = listaIds.get(oRandomService.getRandomInt(0, listaIds.size() - 1));
-        return oCandidaturaRepository.findById(idAleatorio).orElseThrow(() -> new ResourceNotFoundException("Candidatura no encontrada"));
+        return oCandidaturaRepository.findById(idAleatorio)
+                .orElseThrow(() -> new ResourceNotFoundException("Candidatura no encontrada"));
     }
-    
 
-   
 }
